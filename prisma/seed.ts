563c0bd338d97d3de1a2e4444b04340bf5dcf600
yurabezhentsev <yurabezhentsev@gmail.com>
2026-08-29 13:52:45 -0500

    docs(SR-34): reduced session expiration to 60 mins

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -23,7 +23,7 @@ async function main() {
 			data: {
 				userId: mother.id,
 				sessionToken: e2eSessionToken,
-				expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+				expires: new Date(Date.now() + 60 * 60 * 1000),
 			},
 		});
 	}
@@ -41,7 +41,7 @@ async function main() {
 			data: {
 				userId: father.id,
 				sessionToken: secondE2eSessionToken,
-				expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+				expires: new Date(Date.now() + 60 * 60 * 1000),
 			},
 		});
 	}
