ae34ac559bc94e375a6642d97c2693db8c53aab2
yurabezhentsev <yurabezhentsev@gmail.com>
2026-09-08 14:32:41 -0500

    refactor(SR-46): added shared millisecond constants

--- lib/bookings/time.ts
+++ lib/bookings/time.ts
@@ -1,6 +1,6 @@
+import { MS_PER_HOUR } from '@/lib/date';
+
 export const BOOKING_EXPIRY_LEAD_TIME_HOURS = 2;
 
-const MILLISECONDS_PER_HOUR = 60 * 60 * 1_000;
-
 export const getBookingExpiresAt = (scheduledPickupAt: Date) =>
-	new Date(scheduledPickupAt.getTime() - BOOKING_EXPIRY_LEAD_TIME_HOURS * MILLISECONDS_PER_HOUR);
+	new Date(scheduledPickupAt.getTime() - BOOKING_EXPIRY_LEAD_TIME_HOURS * MS_PER_HOUR);
